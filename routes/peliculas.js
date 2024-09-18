1374464d1b1fd5ca41c452e728a2e52b8fccd1b2
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-18 14:35:06 -0300

    Se agrego la ruta para eliminar peliculas

--- routes/peliculas.js
+++ routes/peliculas.js
@@ -1,9 +1,10 @@
 
 const express = require('express');
 const router = express.Router();
-const { getPeliculas, addPelicula } = require('../controllers/peliculasController');
+const { getPeliculas, addPelicula, deletePelicula } = require('../controllers/peliculasController');
 
 router.get('/', getPeliculas);
 router.post('/', addPelicula);
+router.delete('/:id', deletePelicula);
 
 module.exports = router;
