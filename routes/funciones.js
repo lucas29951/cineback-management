7a19cd36010f93751daed6a95ef8eb2e1d6e9b36
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-18 15:12:38 -0300

    Agregada la ruta de eliminacion de funciones

--- routes/funciones.js
+++ routes/funciones.js
@@ -1,9 +1,10 @@
 
 const express = require('express');
 const router = express.Router();
-const { getFunciones, addFuncion } = require('../controllers/funcionesController');
+const { getFunciones, addFuncion, deleteFuncion } = require('../controllers/funcionesController');
 
 router.get('/', getFunciones);
 router.post('/', addFuncion);
+router.delete('/:id', deleteFuncion);
 
 module.exports = router;
