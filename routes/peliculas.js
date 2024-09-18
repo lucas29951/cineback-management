b89ebc65d8c7c6f9c4e3b50784fa3c9c1df49fb4
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-18 14:51:42 -0300

    Ruta definida para actualizar peliculas

--- routes/peliculas.js
+++ routes/peliculas.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
 const router = express.Router();
-const { getPeliculas, addPelicula, deletePelicula } = require('../controllers/peliculasController');
+const { getPeliculas, addPelicula, deletePelicula, updatePelicula } = require('../controllers/peliculasController');
 
 router.get('/', getPeliculas);
 router.post('/', addPelicula);
 router.delete('/:id', deletePelicula);
+router.put('/:id', updatePelicula);
 
 module.exports = router;
