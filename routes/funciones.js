09e497dd8f09e31cd9718f99d8dfe12aa3a730d3
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-19 14:05:06 -0300

    Ruta agregada para el manejo de actualizacion de funciones

--- routes/funciones.js
+++ routes/funciones.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
 const router = express.Router();
-const { getFunciones, addFuncion, deleteFuncion } = require('../controllers/funcionesController');
+const { getFunciones, addFuncion, deleteFuncion, updateFuncion } = require('../controllers/funcionesController');
 
 router.get('/', getFunciones);
 router.post('/', addFuncion);
 router.delete('/:id', deleteFuncion);
+router.put('/:id', updateFuncion);
 
 module.exports = router;
