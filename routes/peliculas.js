2bd08bdb546e525250427923836bfd5d5f534a05
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-30 15:35:58 -0300

    Nueva ruta de peliculas agregada

--- routes/peliculas.js
+++ routes/peliculas.js
@@ -1,9 +1,10 @@
 
 const express = require('express');
 const router = express.Router();
-const { getPeliculas, addPelicula, deletePelicula, updatePelicula } = require('../controllers/peliculasController');
+const { getPeliculas, addPelicula, deletePelicula, updatePelicula, getPeliculaByID } = require('../controllers/peliculasController');
 
 router.get('/', getPeliculas);
+router.get('/:id', getPeliculaByID);
 router.post('/', addPelicula);
 router.delete('/:id', deletePelicula);
 router.put('/:id', updatePelicula);
