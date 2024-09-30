c73d066fcb5287451ebc5f1865bea15e6bdc0233
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-30 16:27:38 -0300

    Ruta agregada para el nuevo metodo - getFuncionesByPelicula

--- routes/funciones.js
+++ routes/funciones.js
@@ -1,9 +1,10 @@
 
 const express = require('express');
 const router = express.Router();
-const { getFunciones, addFuncion, deleteFuncion, updateFuncion } = require('../controllers/funcionesController');
+const { getFunciones, addFuncion, deleteFuncion, updateFuncion, getFuncionesByPelicula } = require('../controllers/funcionesController');
 
 router.get('/', getFunciones);
+router.get('/:id', getFuncionesByPelicula);
 router.post('/', addFuncion);
 router.delete('/:id', deleteFuncion);
 router.put('/:id', updateFuncion);
